ab9150d5ebeda63c7a5714745ce1629a3bb6dc70
Penny Lam <pennylam@live.com>
2025-11-04 22:25:15 +0000

    add ft_strchr

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -10,6 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 char	*ft_strchr(const char *s, int c)
 {
 	int	i;
@@ -18,17 +20,7 @@ char	*ft_strchr(const char *s, int c)
 	while (s[i] != (unsigned char)c && s[i] != '\0')
 		i++;
 	if (s[i] == (unsigned char)c)
-		return ((char *)&s[i]);
+		return (char *)&s[i];
 	else
-		return (NULL);
+		return NULL;
 }
-/*
-int	main()
-{
-const char *test = "hello world";
-
-printf("%s\n", ft_strchr(test, 'e'));
-printf("Real strchr:    %s\n", strchr(test, 'e'));
-return (0);
-}
-*/
