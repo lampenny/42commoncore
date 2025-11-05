831336b0d17217c3716ce5cf4b1f16c6154c6a53
penlam <penlam@c3r1s2.42london.com>
2025-11-05 18:03:01 +0000

    fix norminette errors

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -1,6 +1,15 @@
-#include <stddef.h>
-#include <stdio.h>
-#include <string.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: penlam <penlam@student.42london.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/05 17:53:29 by penlam            #+#    #+#             */
+/*   Updated: 2025/11/05 17:58:44 by penlam           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 char	*ft_strchr(const char *s, int c)
 {
 	int	i;
@@ -9,11 +18,11 @@ char	*ft_strchr(const char *s, int c)
 	while (s[i] != (unsigned char)c && s[i] != '\0')
 		i++;
 	if (s[i] == (unsigned char)c)
-		return (char *)&s[i];
+		return ((char *)&s[i]);
 	else
-		return NULL;
+		return (NULL);
 }
-
+/*
 int	main()
 {
 const char *test = "hello world";
@@ -22,3 +31,4 @@ printf("%s\n", ft_strchr(test, 'e'));
 printf("Real strchr:    %s\n", strchr(test, 'e'));
 return (0);
 }
+*/
