655304441aacbc2399a1ffa8713c0fdc4be562ca
Penny Lam <pennylam@live.com>
2025-11-09 19:27:10 +0000

    Fix makefile

--- libft/libft.h
+++ libft/libft.h
@@ -18,13 +18,15 @@
 int		ft_strlen(char *str);
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
-int		ft_toupper(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
 int		ft_tolower(int c);
+int		ft_toupper(int c);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+int	ft_atoi(const char *nptr);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
 
 #endif
