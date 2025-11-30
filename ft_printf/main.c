90f5d54bd17892014c808688506cf93ae961eb59
Penny Lam <pennylam@live.com>
2025-11-30 22:19:33 +0000

    Add test file

---
+++ ft_printf/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+	char	c = 'X';
+	char	s[] = "abcdefghijklmnopqrstuvwxyz";
+	int	i = 425;
+	short int	j = 17;
+	unsigned int	u = 0xf179U;
+
+	printf("\n Characters: \n");
+	printf("%c\n", c);
+	printf("%3c%3c\n", c, c);
+	printf("%x\n", c);
+
+	printf("\n Strings \n");
+	printf("%s\n", s);
+	printf("%.5s\n", s);
+	printf("%30s\n", s);
+	printf("%20.5s\n", s);
+	printf("%-20.5s\n", s);
+	
+	return 0;
+}
