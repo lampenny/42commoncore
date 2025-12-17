df46c93abe21bf75c9d67c5028d1b3b345029c22
Penny Lam <pennylam@live.com>
2025-12-17 17:50:43 +0000

    Add string

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: penlam <penlam@student.42london.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/30 18:15:41 by penlam            #+#    #+#             */
+/*   Updated: 2025/11/30 19:15:46 by penlam           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 #include <stdarg.h>
 #include <stdio.h>
@@ -5,6 +17,35 @@
 int	ft_putchar(char c)
 {
 	return (write(1, &c, 1));
+}
+
+int	ft_putstr(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i])
+	{
+		ft_putchar(s[i]);
+		i++;
+	}
+	return (i);
+}
+
+int	is_valid_format(char c)
+{
+	return (c == 'c' || c == 's' || c == 'p'
+		|| c == 'd' || c == 'i' || c == 'u'
+		|| c == 'x' || c == 'X' || c == '%');
+}
+
+int	handle_format(char specifier, va_list args)
+{
+	if (specifier == 'c')
+		return (ft_putchar(args));
+	if (specifier == 's')
+		return (ft_putstr(args));
+	return (0);
 }
 
 int	ft_printf(const char *format, ...)
@@ -18,20 +59,21 @@ int	ft_printf(const char *format, ...)
 	i = 0;
 	va_start(args, format);
 	count = 0;
+
 	while (format[i])
 	{
-		if (format[i] != '%' && format[i + 1] != 'c')
+		if (format[i] == '%' && is_valid_format(format[i + 1]))
+		{
+			i++;
+			printed = handle_format(format[i], args);
+			count += printed;
+		}
+		else
 		{
 			printed = ft_putchar(format[i]);
 			count += printed;
-			i = i + 1;
-		} 
-		else
-		{
-			printed = ft_putchar(va_arg(args, int));
-			count += printed;
-			i = i + 2;
 		}
+		i++;
 	}
 	va_end(args);
 	return (count);
@@ -39,13 +81,9 @@ int	ft_printf(const char *format, ...)
 
 int	main(void)
 {
-	int	test1;
-	int	test2;
+	char	*str = "this is my string";
 
-	test1 = ft_printf("char: %c\n", 'A');
-	test2 = printf("char: %c\n", 'A');
-	printf("my ft_printf: %d\n", test1);
-	printf("printf: %d\n", test2);
-
+	ft_printf("my printf - char: %c\n", 'A');
+	ft_printf("my printf - str: %s\n", str);
 	return (0);
 }
